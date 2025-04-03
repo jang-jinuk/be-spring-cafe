d0136ebe0e5439baa478c6ac66e5ebb16ffcb827
jang-jinuk <dlsndifn98@gmail.com>
2025-04-03 10:34:37 +0900

    feat: Reply 엔티티 생성
    - Usr, Article과 연관 관계를 맺어줬습니다.

---
+++ src/main/java/codesquad/codestagram/domain/Reply.java
@@ -0,0 +1,54 @@
+package codesquad.codestagram.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+}
