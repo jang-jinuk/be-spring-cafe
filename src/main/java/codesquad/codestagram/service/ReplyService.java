731e956406668e6fab27141741da05ce36923bc0
jang-jinuk <dlsndifn98@gmail.com>
2025-04-03 12:42:32 +0900

    feat: 댓글 삭제 기능 구현
    - 댓글 삭제 기능을 구현했습니다.
    - 댓글 작성자와 삭제하려는 사용자가 동일하지 않으면 삭제 불가능하도록 구현했습니다.

--- src/main/java/codesquad/codestagram/service/ReplyService.java
+++ src/main/java/codesquad/codestagram/service/ReplyService.java
@@ -1,7 +1,9 @@
 package codesquad.codestagram.service;
 
 import codesquad.codestagram.domain.Reply;
+import codesquad.codestagram.domain.User;
 import codesquad.codestagram.repository.ReplyRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,5 +23,17 @@ public class ReplyService {
 
     public List<Reply> findReplyList(Long articleId) {
         return replyRepository.findAllByArticle_Id(articleId);
+    }
+
+    public boolean removeReply(Long replyId, User loginUser) {
+        Reply reply = replyRepository.findById(replyId)
+                .orElseThrow(() -> new EntityNotFoundException("댓글이 존재하지 않습니다."));
+
+        if (reply.getUser().getId() != loginUser.getId()) {
+            return false;
+        }
+
+        replyRepository.deleteById(replyId);
+        return true;
     }
 }
